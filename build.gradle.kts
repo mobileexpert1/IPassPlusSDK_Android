3a21626db7d6534b30c61b205724971c63dcf8a0
MobileDev1 <appsdev096@gmail.com>
2024-04-19 14:43:15 +0530

    support for gradle version 7.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+buildscript {
+    val agp_version by extra("7.1.1")
+}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 /*allprojects {
     repositories {
@@ -14,7 +17,7 @@
 }*/
 
 plugins {
-    id("com.android.application") version "8.1.4" apply false
+    id("com.android.application") version "7.1.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 //    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
     id("com.android.library") version "8.1.1" apply false
