d10bf60093406193e5102a6d69a088ede7c3ac2e
MobileDev1 <appsdev096@gmail.com>
2024-05-08 01:50:38 +0530

    Updation in gradle file

--- IPassPlusSdk/build.gradle.kts
+++ IPassPlusSdk/build.gradle.kts
@@ -123,7 +123,7 @@ project.afterEvaluate {
                 from(components["release"])
                 groupId = "com.sdk.ipassplussdk"
                 artifactId = "IPassPlusSDK"
-                version = "2.0.13"
+                version = "2.0.14"
             }
         }
     }
