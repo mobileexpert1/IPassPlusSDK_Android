4f7e3fb72a4ae7ebea998bc4b3d9b0069e9acb7c
MobileDev1 <appsdev096@gmail.com>
2024-05-09 20:02:42 +0530

    sdk loaders update version

--- IPassPlusSdk/build.gradle.kts
+++ IPassPlusSdk/build.gradle.kts
@@ -104,7 +104,7 @@ project.afterEvaluate {
                 from(components["release"])
                 groupId = "com.sdk.ipassplussdk"
                 artifactId = "IPassPlusSDK"
-                version = "2.0.19"
+                version = "2.0.20"
             }
         }
     }
